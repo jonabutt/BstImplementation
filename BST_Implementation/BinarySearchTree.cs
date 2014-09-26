09e39d0b87f8a43e52bd22f227477276757d518c
jonabutt <jonathanbuttigieg1@gmail.com>
2014-09-26 23:46:33 +0200

    BST Implementation upload

---
+++ BST_Implementation/BinarySearchTree.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Implementation
+{
+    public class BinarySearchTree<T> where T : IComparable<T>, IEquatable<T>
+    {
+        BstNode<T> root;
+
+        private BstNode<T> CrateBsdNode(T data)
+        {
+            BstNode<T> newNode = new BstNode<T>()
+            {
+                data = data
+            };
+            return newNode;
+        }
+        public void Insert(T data)
+        {
+            root = Insert(root, data);
+        }
+        public bool Search(T data)
+        {
+            return Search(root, data);
+        }
+        private BstNode<T> Insert(BstNode<T> root, T data)
+        {
+            if (root == null)
+            {
+                root = CrateBsdNode(data);
+            }
+            else
+            {
+                if (data.CompareTo(root.data) <= 0)
+                    // root.Value > data, therefore n must be added to the left subtree
+                    root.left = Insert(root.left, data);
+                else
+                    // parent.Value < data, therefore n must be added to the right subtree
+                    root.right = Insert(root.right, data);
+            }
+            return root;
+        }
+
+        public void InsertUsingIterations(T data)
+        {
+            BstNode<T> temp = root;
+            while (temp != null)
+            {
+                if (data.CompareTo(temp.data) <= 0)
+                {
+                    if (temp.left == null)
+                        break;
+                    temp = temp.left;
+                }
+                else
+                {
+                    if (temp.right == null)
+                        break;
+                    temp = temp.right;
+                }
+            }
+            if (temp == null)
+            {
+                root = CrateBsdNode(data);
+            }
+            else if (data.CompareTo(temp.data) <= 0)
+            {
+                temp.left = CrateBsdNode(data);
+            }
+            else
+            {
+                temp.right = CrateBsdNode(data);
+            }
+        }
+
+        public bool SeachUsingIteration(T data)
+        {
+            BstNode<T> temp = root;
+            while (temp != null)
+            {
+                if (temp.data.CompareTo(data) == 0)
+                    return true;
+                else if (temp.data.CompareTo(data) == -1)
+                    temp = temp.right;
+                else
+                    temp = temp.left;
+            }
+            return false;
+        }
+
+
+        private bool Search(BstNode<T> root, T data)
+        {
+            if (root == null) return false;
+            else if (root.data.CompareTo(data) == 0) return true;
+            else if (root.data.CompareTo(data) == -1) return Search(root.right, data);
+            else return Search(root.left, data);
+        }
+    }
+
+    public class BstNode<T>
+    {
+        public T data;
+        public BstNode<T> left;
+        public BstNode<T> right;
+    }
+}
---
+++ TestingConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BST_Implementation;
+namespace TestingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinarySearchTreeInt bst = new BinarySearchTreeInt();
+            BinarySearchTree<int> bst = new BinarySearchTree<int>();
+            bst.InsertUsingIterations(80);
+            bst.InsertUsingIterations(20);
+            bst.InsertUsingIterations(90);
+            bst.InsertUsingIterations(80);
+            bst.InsertUsingIterations(25);
+            bst.InsertUsingIterations(340);
+            bst.InsertUsingIterations(50);
+            string userValue = Console.ReadLine();
+            int val = Int32.Parse(userValue);
+            if (bst.SeachUsingIteration(val))
+                Console.WriteLine("Found");
+            else
+                Console.WriteLine("Not found");
+            Console.ReadKey();
+        }
+    }
+}
